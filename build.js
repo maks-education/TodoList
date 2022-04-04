f61374d66d2f5f6e6c47ba6ea0a76ef6177cc46f
studentmak <586947>
2022-04-04 20:29:30 +0300

    HELP PLEASE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- build.js
+++ build.js
@@ -2,8 +2,11 @@ const { exec } = require('child_process');
 const fs  = require('fs')
 const path = require('path')
 
-let child1 = exec('cd ./client && npm i && npm run build' , () => {
-    // fs.renameSync(path.join(__dirname, './client/dist'), path.join(__dirname, './server/dist'));
+let child1 = exec('cd ./client && npm i && npm run build' , (error) => {
+if (error) {
+    console.error(error)
+}
+    fs.renameSync(path.join(__dirname, './client/dist'), path.join(__dirname, './server/dist'));
 
     let child2 = exec('cd ./server && npm i && npm run prod' , () => {  
 })
@@ -11,3 +14,6 @@ child2.stdout.pipe(process.stdout)
 })
 
 child1.stdout.pipe(process.stdout) 
+
+
+
