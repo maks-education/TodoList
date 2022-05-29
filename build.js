99c9f4fe4d553bce0d379945d3566faa438d58b3
Roman Rodionov <roman_rodionov@epam.com>
2022-05-29 19:45:33 +0200

    - test

--- build.js
+++ build.js
@@ -1,12 +1,18 @@
 const { exec } = require('child_process');
 console.log(process.env.PORT)
-let child1 = exec('cd ./client && npm i && npm run build && cd .. && node ./copyDist.js && cd ./server && npm i && npm run prod' , (error) => {
-if (error) {
-    console.error(error)
+function build(callback) {
+    let child1 = exec('cd ./client && npm i && npm run build && cd .. && node ./delete_dist.js && node ./copyDist.js && cd ./server && npm i && npm run prod' , (error) => {
+        if (error) {
+            console.error(error)
+        }
+        callback()
+    })
+    child1.stdout.pipe(process.stdout)
 }
+
+
+
+build(function () {
+    console.log('Build successful, application started')
 })
-
-child1.stdout.pipe(process.stdout) 
-
-
 
