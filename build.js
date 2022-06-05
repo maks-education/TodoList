4dfa183eecdd95958ac9f92499da460060a53ffc
Roman Rodionov <roman_rodionov@epam.com>
2022-06-05 20:09:39 +0200

    -test

--- build.js
+++ build.js
@@ -1,23 +1,12 @@
 const { exec } = require('child_process');
 
 function build(callback) {
-    let child = exec('npm install forever -g && cd ./server && npm i && npm run prod' , (error) => {
+    let child = exec('cd ./client && npm i && npm run build && cd .. && node ./delete_dist.js && node ./copyDist.js && cd ./server && npm i' , (error) => {
         if (error) {
             console.error(error)
-        } else {
-            console.log('Server built successful')
-            let child2 = exec('cd ./client && npm i && npm run build && cd .. && node ./delete_dist.js && node ./copyDist.js', () => {
-                if (error) {
-                    console.error(error)
-                } else {
-                    console.log('Client built successful')
-                    callback()
-                }
-            })
-            child2.stdout.pipe(process.stdout);
-            child2.stderr.pipe(process.stderr);
         }
-    });
+        callback()
+    })
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
 }
