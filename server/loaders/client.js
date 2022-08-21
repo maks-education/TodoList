a5ce69dfd2ed7e186e0fa8df447f8c49313cf0b8
Roman Rodionov <roman_rodionov@epam.com>
2022-08-21 16:40:36 +0200

    - fixed db encryption error on heroku

--- server/loaders/client.js
+++ server/loaders/client.js
@@ -3,18 +3,24 @@ import Sequelize from "sequelize";
 let sequelize;
 
 export function createDBClient() {
-    sequelize = new Sequelize(
-        process.env.DATABASE_URL
-            ? `postgres://${process.env.DATABASE_URL}`
-            : {
-                dialect: 'postgres',
-                username: process.env.DB_USER,
-                host: process.env.DB_HOST,
-                database: process.env.DB_NAME,
-                password: process.env.DB_PSW,
-                port: process.env.DB_PORT
-            }
-    );
+    sequelize = process.env.DATABASE_URL
+        ? new Sequelize(
+            process.env.DATABASE_URL,
+            {
+                dialectOptions: {
+                    ssl: {
+                        rejectUnauthorized: false
+                    }
+                }
+            })
+        : new Sequelize({
+            dialect: 'postgres',
+            username: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PSW,
+            port: process.env.DB_PORT
+        });
     return sequelize;
 }
 
